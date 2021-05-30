2da5549d5167b7d94ee68f6bd2a881b8eaf005ab
jonah0502 <jonah0502@gmail.com>
2021-05-29 19:36:30 -0700

    implimented method where if a number is divisible by both 5 and 3 it prints fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
 def fizzbuzz(val):
-    if(val %3 == 0):
+    if(val%3 == 0 and val%5 == 0):
+        return "FizzBuzz"
+    elif(val %3 == 0):
         return "Fizz"
     elif (val %5 == 0):
         return "Buzz"
