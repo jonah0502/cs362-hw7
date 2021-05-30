7b716ea21c2518fe4892b3a195eba354f5f1bec1
jonah0502 <jonah0502@gmail.com>
2021-05-29 23:11:41 -0700

    added method for if divisible by 400

--- leapYear.py
+++ leapYear.py
@@ -1,7 +1,8 @@
 def isLeapYear(year):
     if (year % 4) == 0:
         if (year % 100) == 0:
-            pass
+            if (year % 400) == 0:
+                return True
         else:
             return True
     else:
