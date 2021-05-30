c5a06c1509211e4a6b0e51109b8e087c5fa7d908
jonah0502 <jonah0502@gmail.com>
2021-05-29 19:35:25 -0700

    added third test which checks if 15 prints fizzbuzz

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -8,6 +8,9 @@ class fizzbuzzTestCase(unittest.TestCase):
     def test_2(self):
         self.assertEqual(fizzbuzz.fizzbuzz(5),"Buzz")
     
+    def test_3(self):
+        self.assertEqual(fizzbuzz.fizzbuzz(15),"FizzBuzz")
+    
     
 
 if __name__ == "__main__":
