3717e2cfa143c7e759ad69bc2166930639ead23c
jonah0502 <jonah0502@gmail.com>
2021-05-29 19:25:03 -0700

    wrote second test case testing if 5 gives buzz

--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -2,9 +2,13 @@ import fizzbuzz
 import unittest
 
 class fizzbuzzTestCase(unittest.TestCase):
-    def test_divisibleByThree(self):
+    def test_1(self):
         self.assertEqual(fizzbuzz.fizzbuzz(3),"Fizz")
 
+    def test_2(self):
+        self.assertEqual(fizzbuzz.fizzbuzz(5),"Buzz")
+    
+    
 
 if __name__ == "__main__":
     unittest.main()
