eb43424a38240c448d4c112b2c77b3f6c9bc892f
jonah0502 <jonah0502@gmail.com>
2021-05-29 22:56:59 -0700

    changed first test to 2150

--- leapYear_test.py
+++ leapYear_test.py
@@ -3,7 +3,7 @@ import unittest
 
 class leapyearTestCase(unittest.TestCase):
     def test_normalyear(self):
-        self.assertEqual(leapYear.isLeapYear(1),True)
+        self.assertEqual(leapYear.isLeapYear(2150),True)
         
 if __name__ == "__main__":
     unittest.main()
