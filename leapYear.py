cb99537cb9c54da0bbe7a8029f28aff768f9a00f
jonah0502 <jonah0502@gmail.com>
2021-05-29 22:59:35 -0700

    implimented not divisible by 4 return false method

--- leapYear.py
+++ leapYear.py
@@ -1,1 +1,5 @@
-
+def isLeapYear(year):
+    if (year % 4) == 0:
+        pass
+    else:
+        return False
--- leapYear_test.py
+++ leapYear_test.py
@@ -2,8 +2,8 @@ import leapYear
 import unittest
 
 class leapyearTestCase(unittest.TestCase):
-    def test_normalyear(self):
-        self.assertEqual(leapYear.isLeapYear(2150),True)
+    def test_leapYear(self):
+        self.assertEqual(leapYear.isLeapYear(2150),False)
         
 if __name__ == "__main__":
     unittest.main()
